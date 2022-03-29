8abfbbf758e448542851e15fb40b54271ff2e454
Haik Ivanyan <samplelogo19@gmail.com>
2022-03-29 21:06:36 +0300

    done 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const imageMarkup = createGallery(galleryItems);
 
-console.log(galleryItems);
+function createGallery(galleryItems) {
+  return galleryItems
+    .map(({ original, description, preview }) => {
+      return `<div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>`;
+    })
+    .join('');
+}
+galleryRef.insertAdjacentHTML('beforeend', imageMarkup);
+galleryRef.addEventListener('click', onGalleryClick);
+function onGalleryClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  modalShow(e.target.dataset.source);
+}
+function modalShow(src) {
+  const instance = basicLightbox.create(`
+    <img src="${src}" width="800" height="600">`);
+  instance.show();
+
+  window.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
